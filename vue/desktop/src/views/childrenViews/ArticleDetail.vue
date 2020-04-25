e0f80aae57b3e156c6346baeb7074544d0555312
zhoususu <431185521@qq.com>
2020-04-26 02:09:22 +0800

    fix: 移动端富文本图片样式

--- vue/desktop/src/views/childrenViews/ArticleDetail.vue
+++ vue/desktop/src/views/childrenViews/ArticleDetail.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="news-detail">
+  <div class="article-detail">
     <article class="news-article">
       <h1 class="article__title">{{ article.title }}</h1>
       <el-row type="flex" class="article__meta sub-font_sub" :gutter="1">
@@ -295,6 +295,11 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.article-detail {
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+  'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+}
+
 .icon-left {
   margin-right: 2px;
 }
@@ -353,6 +358,7 @@ export default {
 }
 .article__content {
   margin-top: 50px;
+  line-height: 1.75;
 }
 .comment__main,
 .reply__main {
